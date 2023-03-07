18abdc10f61492ced766cfac809155c4e989771e
Fernando Ureta <fernandoureta1995@gmail.com>
2023-03-07 20:34:18 -0300

    Se agregaron 2 spawn points, con 1 script nuevo para cada 1 para manejar el spawn de las waves, tambien se le pusieron a los enemigos de las 3 primeras waves, se le puso tag de Enemy (nuevo tag) a la slime por error, correjir luego

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -10,7 +10,7 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(1))
+        if(Input.GetMouseButtonDown(0))
         {
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
@@ -21,4 +21,5 @@ public class Movement : MonoBehaviour
             }
         }
     }
+
 }
---
+++ Assets/Scripts/WaveSpawner1.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using UnityEngine;
+
+public class WaveSpawner1 : MonoBehaviour
+{
+
+    public enum SpawnState { SPAWNING, WAITING, COUNTING};
+
+    [System.Serializable]
+    public class Wave
+    {
+        public string name;
+        public Transform enemy;
+        public int size;
+        public float spawnRate;
+    }
+
+    public Wave[] waves;
+    private int nextWave = 0;
+
+    public float timeBetweenWaves = 5f;
+    public float waveCountdown;
+
+    private SpawnState state = SpawnState.COUNTING;
+
+    void Start()
+    {
+        waveCountdown = timeBetweenWaves;
+    }
+
+    void Update ()
+    {
+
+        if (state == SpawnState.WAITING)
+        {
+            WaveCompleted();
+        }
+
+        if (waveCountdown <= 0)
+        {
+            if (state != SpawnState.SPAWNING)
+            {
+                StartCoroutine( SpawnWave ( waves[nextWave] ) );
+            }
+        }
+        else
+        {
+            waveCountdown -= Time.deltaTime;
+        }
+    }
+
+    void WaveCompleted()
+    {
+        Debug.Log("Wave Completed");
+
+        state = SpawnState.COUNTING;
+        waveCountdown = timeBetweenWaves;
+
+        if (nextWave + 1 > waves.Length - 1)
+        {
+            nextWave = 0;
+            waveCountdown = 5000f;
+            Debug.Log ("Waves completed! Looping...");
+        } 
+
+        else
+        {
+            nextWave++;
+        }
+
+    }
+    
+    IEnumerator SpawnWave (Wave _wave)
+    {
+
+        Debug.Log("Spawning Wave: "+ _wave.name);
+        state = SpawnState.SPAWNING;
+
+        for (int i = 0; i < _wave.size; i++)
+
+        {
+            SpawnEnemy (_wave.enemy);
+            yield return new WaitForSeconds (_wave.spawnRate/1f);
+        }
+
+        state = SpawnState.WAITING;
+
+        yield break;
+    }
+
+    void SpawnEnemy (Transform _enemy)
+    {
+        Debug.Log("Spawning enemy: " + _enemy.name);
+        Instantiate (_enemy, transform.position, transform.rotation);
+    }
+}
